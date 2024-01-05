35e638f73d79feb12110d51574c8d5bac03e8459
Seojin Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2024-01-05 10:36:42 +0900

    #1 feat: modulize api function (#4)
    * sdp-tech#1 feat: create api request module
    
    * chore: remove strings.xml from gitignore

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: 'react-native-dotenv',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: true,
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
---
+++ src/common/requests.js
@@ -0,0 +1,146 @@
+import { UPCY_API_URL } from 'react-native-dotenv';
+import axios from 'axios';
+import { Alert } from 'react-native';
+import { getAccessToken, getRefreshToken, removeAccessToken, setAccessToken } from './storage';
+
+export default function Request() {
+  const defaultRequest = async (path, body) => {
+    const accessToken = await getAccessToken();
+    const refreshToken = await getRefreshToken();
+
+    const url = UPCY_API_URL + path;
+
+    let headerValue;
+
+    if (accessToken === null || accessToken === undefined || accessToken === false) {
+      headerValue = 'No Auth';
+    } else {
+      headerValue = `Bearer ${accessToken}`;
+    }
+
+    console.log(headerValue)
+
+    try {
+      const response = await body(url, headerValue);
+      return response;
+    } catch (err) {
+      if (err.response === undefined) {
+        // 백엔드와 통신 자체가 실패(ERR_CONNECTION_REFUSED)
+        console.log(err);
+        Alert.alert('ERR_CONNECTION_REFUSED' + UPCY_API_URL);
+        console.warn(UPCY_API_URL);
+        throw err;
+      } else if (err.response.status === 401) {
+        // access 토큰이 만료된 경우 또는 로그인이 필요한 기능의 경우
+        // 만료된 토큰 : "Given token not valid for any token type"
+        // 없는 토큰 : "자격 인증데이터(authentication credentials)가 제공되지 않았습니다."
+        removeAccessToken(); // 기존 access token 삭제
+        // refresh 토큰을 통해 access 토큰 재발급
+        try {
+          const response = await axios.post(
+            UPCY_API_URL + '/users/token/refresh/',
+            {
+              refresh: refreshToken,
+            },
+            {
+              headers: {
+                Authorization: 'No Auth',
+              },
+            },
+          );
+          setAccessToken(response.data.access);
+          console.warn('new access token', response.data.access);
+          headerValue = `Bearer ${response.data.access}`;
+        } catch (error) {
+          // refresh 토큰이 유효하지 않은 모든 경우(토큰 만료, 토큰 없음 등)
+          console.error(error);
+          return;
+        }
+        console.log('==============');
+        // 새로운 access 토큰으로 API 요청 재수행
+        const response = await body(url, headerValue);
+        console.log('data?', response);
+        return response;
+      } else {
+        // 백엔드와 통신 자체는 성공, status code가 정상 값 범위 외
+        console.log(err);
+        return err.response;
+      }
+    }
+  };
+
+  const get = async (path, params, headers) => {
+    return await this.defaultRequest(path, async (url, headerValue) => {
+      const response = await axios.get(url, {
+        params: params,
+
+        headers: {
+          Authorization: headerValue,
+          ...headers,
+        },
+      });
+      console.log('request test => ', response);
+      return response;
+    });
+  };
+
+  const post = async (path, data, headers) => {
+    return await defaultRequest(path, async (url, headerValue) => {
+      const response = await axios.post(url, data, {
+        headers: {
+          Authorization: headerValue,
+          ...headers,
+        },
+      });
+      console.log('headers', {
+        Authorization: headerValue,
+        ...headers,
+      });
+      console.log('request test => ', response);
+      return response;
+    });
+  };
+
+  const put = async (path, data, headers) => {
+    return await defaultRequest(path, async (url, headerValue) => {
+      const response = await axios.put(url, data, {
+        headers: {
+          Authorization: headerValue,
+          ...headers,
+        },
+      });
+      console.log('request test => ', response);
+      return response;
+    });
+  };
+
+  const del = async (path, params, headers) => {
+    return await defaultRequest(path, async (url, headerValue) => {
+      const response = await axios.delete(url, {
+        params: params,
+
+        headers: {
+          Authorization: headerValue,
+          ...headers,
+        },
+      });
+      console.log('request test => ', response);
+      return response;
+    });
+  };
+
+  const patch = async (path, data, headers) => {
+    return await defaultRequest(path, async (url, headerValue) => {
+      const response = await axios.patch(url, data, {
+        headers: {
+          Authorization: headerValue,
+          ...headers,
+        },
+      });
+      console.log('request test => ', response);
+      return response;
+    });
+  };
+
+  return { get, post, put, del, patch };
+}
