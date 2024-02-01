4b02f00c5d35a3ff6e96c211be9b141a43f9d493
Seojin Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2024-02-02 00:04:31 +0100

    sdp-tech#25 feat: add quotation form

---
+++ src/common/CheckBox.tsx
@@ -0,0 +1,35 @@
+import { View, TouchableOpacity, ViewStyle } from 'react-native';
+import Check from '../assets/common/Check.svg';
+import { PURPLE } from '../styles/GlobalColor';
+import { Body14M } from '../styles/GlobalText';
+
+interface CheckBoxProps {
+  pressed: boolean;
+  onPress: () => void;
+  text: string;
+  style?: ViewStyle
+}
+
+const CheckBox = ({ pressed, onPress, text, style }: CheckBoxProps) => {
+  return (
+    <View style={{flexDirection: 'row', ...style}}>
+      <TouchableOpacity
+        onPress={onPress}
+        style={{
+          width: 20, 
+          height: 20, 
+          borderWidth: 1, 
+          borderColor: PURPLE, 
+          backgroundColor: pressed ? PURPLE : 'white',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Check />
+      </TouchableOpacity>
+      <Body14M style={{marginLeft: 10}}>{text}</Body14M>
+    </View>
+  )
+}
+
+export default CheckBox;
