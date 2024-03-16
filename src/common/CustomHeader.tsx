adb66a64b60149aed05e7bd5dae63620a80dc770
KM <penguinman12@snu.ac.kr>
2024-03-16 16:08:44 +0900

    add useEffect

--- src/common/CustomHeader.tsx
+++ src/common/CustomHeader.tsx
@@ -68,6 +68,10 @@ const CustomHeader = ({ onSearch, onAlarm, }: CustomHeaderProps) => {
   
   const [selectedTab, setSelectedTab] = useState<'Goods' | 'Market'>('Goods');
  
+  useEffect(() =>{
+    setSelectedTab(selectedTab);
+  }, [selectedTab])
+  
   //눌렀을 때 색 변경 및 font weight,size 변경 안 됨?
   return (<>
 
