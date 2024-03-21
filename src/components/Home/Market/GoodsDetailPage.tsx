6a7b2d90592cb58f822bd95a125fdd1f3ffbbb3c
Dindb-dong <dongwook443@yonsei.ac.kr>
2024-03-21 13:26:31 +0900

    #14 feat : reformed goods detail page

--- src/components/Home/Market/GoodsDetailPage.tsx
+++ src/components/Home/Market/GoodsDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Dimensions, SafeAreaView, ScrollView, Text, TouchableOpacity, View, StyleSheet, useWindowDimensions } from 'react-native';
+import { Dimensions, SafeAreaView, ScrollView, Text, TouchableOpacity, View, StyleSheet, useWindowDimensions, Image, ImageBackground } from 'react-native';
 import { StackScreenProps, createStackNavigator } from '@react-navigation/stack';
 import { HomeStackParams } from '../../../pages/Home';
 import Arrow from '../../../assets/common/Arrow.svg';
@@ -27,60 +27,78 @@ const GoodsDetailPageScreen = ({ navigation, route }: StackScreenProps<HomeStack
       screenOptions={() => ({
         headerShown: false,
       })}>
-      <DetailPageStack.Screen name='DetailPage' component={GoodsDetailPageMainScreen}/>
+      <DetailPageStack.Screen name='DetailPage' component={GoodsDetailPageMainScreen} />
     </DetailPageStack.Navigator>
   );
 };
 
-const ProfileSection = ({ navigation }: {navigation:any}) => {
+const ProfileSection = ({ navigation }: { navigation: any }) => {
   const [data, setData] = useState([]);
   return (
-    <View>
-    <View style={{flexDirection: "row", height: 50, alignItems: 'center', justifyContent:"space-between"}}>
-        <TouchableOpacity onPress={() => {
-          navigation.goBack();
-        }}>
-          <Arrow color='black' />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          navigation.goBack();
-        }}>
-          <Search stroke={"black"}/>
-        </TouchableOpacity>
-      </View>
-      <CardView // 데이터 들어오면 렌더링
-        gap={0}
-        offset={0}
-        data={data}
-        pageWidth={width}
-        dot={true}
-        renderItem={({ item }: any) => (
-          <View style={{width: width, height: height * 0.4}}><UnFilledLike color={'black'}/></View>
-          // <CurationItemCard
-          //   rep={true}
-          //   data={item}
-          //   style={{ width: width, height: height * 0.4 }}
-          // />
-        )}
-      />
+    <ScrollView>
+      <View>
+        <View style={{ flexDirection: "column", height: 80, alignItems: 'center', justifyContent: "space-between" }}>
+          <View style={ // To see the top line
+            {
+              width: '100%', height: 30,
+              backgroundColor: '#00000066',
+              opacity: 0.5
+            }} />
+          <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+            <TouchableOpacity onPress={() => {
+              navigation.goBack();
+            }}>
+              <Arrow color='black' />
+            </TouchableOpacity>
+            <View // The justifyContent didn't work, so I put this
+              style={{ width: '80%' }} />
+            <TouchableOpacity onPress={() => {
+              navigation.goBack();
+            }}>
+              <Search stroke={"black"} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <CardView // 데이터 들어오면 렌더링
+          gap={0}
+          offset={0}
+          data={data}
+          pageWidth={width}
+          dot={true}
+          renderItem={({ item }: any) => (
+            <View style={{ width: width, height: height * 0.4 }}><UnFilledLike color={'black'} /></View>
+            // <CurationItemCard
+            //   rep={true}
+            //   data={item}
+            //   style={{ width: width, height: height * 0.4 }}
+            // />
+          )}
+        />
         {/* 컴포넌트로 변경 예정 */}
-        <View style={TextStyles.borderBottom}>
-          <Text style={TextStyles.Sub}>#키워드 #키워드 #키워드 #키워드 #키워드</Text>
+        <View style={TextStyles.borderBottom1}>
+          <ImageBackground // 임시 이미지 
+            style={{ width: '100%', height: 160 }}
+            imageStyle={{ height: 160 }}
+            source={{ uri: 'https://image.made-in-china.com/2f0j00efRbSJMtHgqG/Denim-Bag-Youth-Fashion-Casual-Small-Mini-Square-Ladies-Shoulder-Bag-Women-Wash-Bags.webp' }}>
+            <View style={{ width: '100%', height: 160, backgroundColor: '#00000066', opacity: 0.7 }} />
+          </ImageBackground>
+          <Text style={TextStyles.Sub}>#키워드 #키워드 #키워드 #키워드</Text>
           <Text style={TextStyles.Title}>상품 이름</Text>
           <Text style={TextStyles.PriceInfo}>기본: <Text style={TextStyles.Price}>20000원</Text></Text>
           <Text style={TextStyles.PriceInfo}>최대: <Text style={TextStyles.Price}>20000원</Text></Text>
         </View>
         {/* 컴포넌트로 변경 예정 */}
-        <View style={{...TextStyles.borderBottom, justifyContent: 'space-between'}}>
-          <View style={{ padding:15, flexDirection:'row'}}>
-            <View style={{backgroundColor:"gray", width:50, height:50, borderRadius:25}}></View>
-            <View style={{marginLeft: 20, justifyContent:'center'}}>
-              <Text style={{fontSize:16, padding: 3, fontWeight:'700', color:'#000'}}>마켓명 &gt;</Text>
-              <Text style={{fontSize:16, padding: 3, color:'#000'}}>리폼러닉네임</Text>
+        <View style={{ ...TextStyles.borderBottom2, justifyContent: 'space-between' }}>
+          <View style={{ padding: 15, flexDirection: 'row' }}>
+            <View style={{ backgroundColor: "gray", width: 50, height: 50, borderRadius: 25 }}></View>
+            <View style={{ marginLeft: 20, justifyContent: 'center' }}>
+              <Text style={{ fontSize: 16, padding: 3, fontWeight: '700', color: '#000' }}>마켓명 &gt;</Text>
+              <Text style={{ fontSize: 16, padding: 3, color: '#000' }}>리폼러닉네임</Text>
             </View>
           </View>
         </View>
-        </View>
+      </View>
+    </ScrollView>
   )
 }
 
@@ -88,14 +106,14 @@ const GoodsDetailPageMainScreen = ({ navigation }: StackScreenProps<DetailPageSt
   const layout = useWindowDimensions();
   const [index, setIndex] = useState<number>(0);
   const [routes] = useState([
-    { key: 'detail', title: '상세설명'},
+    { key: 'detail', title: '상세설명' },
     { key: 'option', title: '필독사항' },
-    { key: 'review', title: '리뷰'}
+    { key: 'review', title: '리뷰' }
   ]);
-  
+
   return (
-    <View style={{flex:1}}>
-        <Tabs.Container
+    <View style={{ flex: 1 }}>
+      <Tabs.Container
         renderHeader={props => <ProfileSection navigation={navigation} />}
         headerContainerStyle={{
           shadowOpacity: 0,
@@ -113,22 +131,22 @@ const GoodsDetailPageMainScreen = ({ navigation }: StackScreenProps<DetailPageSt
               backgroundColor: 'white',
             }}
             labelStyle={{
-              color: BLACK,
+              color: '#222222',
               fontWeight: '700',
-              fontSize: 16
+              fontSize: 14
             }}
           />
         )}
       >
         {routes.map(route =>
-          (<Tabs.Tab key={route.key} name={route.title}>
-            {route.key === 'detail' && <DetailBox />}
-            {route.key === 'option' && <OptionBox />}
-            {route.key === 'review' && <ReviewPage />}
-          </Tabs.Tab>)
+        (<Tabs.Tab key={route.key} name={route.title}>
+          {route.key === 'detail' && <DetailBox />}
+          {route.key === 'option' && <OptionBox />}
+          {route.key === 'review' && <ReviewPage />}
+        </Tabs.Tab>)
         )}
       </Tabs.Container>
-      <Footer/>
+      <Footer />
     </View>
   )
 }
@@ -137,29 +155,38 @@ const TextStyles = StyleSheet.create({
   Title: {
     // fontFamily:"Inter",
     padding: 10,
-    color: '#000',
-    fontWeight: "600",
-    fontSize: 24,
-    lineHeight: 28,
+    color: '#222222',
+    fontWeight: "700",
+    fontSize: 18,
+    lineHeight: 24,
     width: "100%",
   },
   Sub: {
     width: "70%",
     fontWeight: "400",
-    padding: 10,
+    paddingLeft: 10,
+    paddingTop: 10,
+    paddingRight: 10,
     fontSize: 14,
     color: "#612EFE",
   },
-  PriceInfo:{
+  PriceInfo: {
     fontWeight: "600",
-    fontSize: 16,
-    padding:10,
+    fontSize: 14,
+    paddingLeft: 10,
+    paddingTop: 5,
+    paddingRight: 10,
+    paddingBottom: 5,
+    color: 'rgba(34, 34, 34, 0.50)',
   },
   Price: {
-    fontWeight: "600",
+    fontWeight: "700",
     fontSize: 16,
-    padding:10,
-    color: '#000',
+    paddingLeft: 10,
+    paddingTop: 5,
+    paddingRight: 10,
+    paddingBottom: 5,
+    color: '#222222',
   },
   recommend: {
     color: "#FFFFFF",
@@ -171,9 +198,13 @@ const TextStyles = StyleSheet.create({
     overflow: "hidden",
     marginTop: 5,
   },
-  borderBottom: {
-    borderBottomWidth:5,
-    borderBlockColor:"#dcdcdc"
+  borderBottom1: {
+    borderBottomWidth: 1,
+    borderBlockColor: "#dcdcdc"
+  },
+  borderBottom2: {
+    borderBottomWidth: 6,
+    borderBlockColor: "#dcdcdc"
   }
 });
 
