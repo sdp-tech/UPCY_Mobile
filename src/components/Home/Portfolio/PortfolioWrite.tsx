b30184617df7dd8acd216e2b1e8b89a949c64f99
eujin-shin <eujinshin0124@gmail.com>
2024-04-10 20:43:26 +0900

    #61 feat: add DetailPageHeader component

--- src/components/Home/Portfolio/PortfolioWrite.tsx
+++ src/components/Home/Portfolio/PortfolioWrite.tsx
@@ -1,6 +1,7 @@
 import { SafeAreaView } from 'react-native';
 import { AddPortfolioProps } from './AddPortfolio';
 import { Body14B } from '../../../styles/GlobalText';
+import DetailScreenHeader from '../components/DetailScreenHeader';
 
 interface WriteScreenParams extends AddPortfolioProps {}
 
@@ -10,6 +11,15 @@ export default function PortfolioWrite({
 }: WriteScreenParams) {
   return (
     <SafeAreaView>
+      <DetailScreenHeader
+        title="포트폴리오 작성"
+        leftButton="LeftArrow"
+        rightButton="Save"
+        onPressLeft={() => {
+          navigation.getParent()?.navigate('Home');
+        }}
+        onPressRight={() => {}}
+      />
       <Body14B>포트폴리오 작성하기</Body14B>
     </SafeAreaView>
   );
