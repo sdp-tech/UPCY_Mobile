d662d12eb8efd5621c3369f3f36c1ea2dfcb8acf
eujin-shin <eujinshin0124@gmail.com>
2024-04-12 22:28:52 +0900

    #61 feat: useObjectUpdater typechecking

--- src/hooks/useObjectUpdater.ts
+++ src/hooks/useObjectUpdater.ts
@@ -1,9 +1,10 @@
 import { Dispatch, SetStateAction, useCallback } from 'react';
 
-export default function useObjectUpdater<T>(
+export default function useObjectUpdater<T extends object>(
   setForm: Dispatch<SetStateAction<T>>,
 ) {
-  return useCallback((v: any, t: string) => {
+  // value의 타입을 한정할 방법은 찾지 못함
+  return useCallback((v: any, t: keyof T) => {
     setForm(prev => {
       return { ...prev, [t]: v };
     });
