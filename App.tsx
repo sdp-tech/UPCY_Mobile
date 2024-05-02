b8f82b2f12a4a35ad4ca7fb228cd5a79144f0480
김동욱 <157615776+Dindb-dong@users.noreply.github.com>
2024-05-02 13:29:23 +0900

    #14 chore: restore from ui_fix
    * #14 feat : fixed goods detail page, added review.svg file
    
    * feat/14/ui_fix: fixed DetailBox (added Review at the bottom), reformed GoodsDetailBox, made NEW Reviews.tsx
    
    * 1. UI fix
    2. List select/delete logic
    
    * 4.8
    
    * #14/이전에 삭제됐던 것들

--- App.tsx
+++ App.tsx
@@ -19,6 +19,7 @@ import HomeIcon from './src/assets/navbar/Home.svg';
 import MyPageIcon from './src/assets/navbar/MyPage.svg';
 import SignIn from './src/components/Auth/SignIn';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { BottomBarProvider, useBottomBar } from './contexts/BottomBarContext';
 
 export type StackProps = {
   Home: undefined;
@@ -37,17 +38,19 @@ const GlobalTheme = {
 
 function App(): React.JSX.Element {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer theme={GlobalTheme}>
-        <Stack.Navigator
-          screenOptions={() => ({
-            headerShown: false,
-          })}>
-          <Stack.Screen name="Home" component={HomeTab} />
-          <Stack.Screen name="Signin" component={SignIn} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </GestureHandlerRootView>
+    <BottomBarProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <NavigationContainer theme={GlobalTheme}>
+          <Stack.Navigator
+            screenOptions={() => ({
+              headerShown: false,
+            })}>
+            <Stack.Screen name="Home" component={HomeTab} />
+            <Stack.Screen name="Signin" component={SignIn} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </GestureHandlerRootView>
+    </BottomBarProvider>
   );
 }
 
@@ -59,6 +62,11 @@ export type TabProps = {
 const CustomTab = ({ state, descriptors, navigation }: BottomTabBarProps) => {
   const loginGuard = useLoginGuard();
   const { options } = descriptors;
+  const { isVisible } = useBottomBar(); // 바텀바 보임/숨김 상태를 가져옵니다.
+
+  if (!isVisible) {
+    return null; // isVisible이 false일 경우 탭바를 렌더링하지 않습니다.
+  }
 
   return (
     <View
