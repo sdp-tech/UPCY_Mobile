5d897e935d0d036f1e4599b505554f6238b87ed6
Eujin Shin <88435970+eujin-shin@users.noreply.github.com>
2024-05-22 20:55:46 +0900

    #61 feat: add ImageCarousel component (#71)
    * #61 feat: add ImageCarousel component
    
    * #61 fix: ImageCarousel rerender as image array updates

--- src/hooks/useImagePicker.ts
+++ src/hooks/useImagePicker.ts
@@ -141,10 +141,7 @@ export function useImagePicker(
   return [handleAddButtonPress, handleImagePress];
 }
 
-export function useImagePickers(
-  setPhotos: Dispatch<SetStateAction<PhotoType[]>>,
-  max: number,
-) {
+export function useImagePickers(max: number) {
   const CameraActions: Action[] = [
     //카메라 & 갤러리 세팅
     {
@@ -218,7 +215,10 @@ export function useImagePickers(
     [],
   );
 
-  const handleAddButtonPress = (images: PhotoType[]) => {
+  const handleAddButtonPress = (
+    images: PhotoType[],
+    callback: (newPhotos: PhotoType[]) => void,
+  ) => {
     const newImages = images;
 
     Alert.alert('사진 선택', '', [
@@ -232,7 +232,7 @@ export function useImagePickers(
               ...CameraActions[0].options,
             },
             selectedPhotos => {
-              setPhotos(newImages.concat(selectedPhotos));
+              callback(newImages.concat(selectedPhotos));
             },
           ),
       },
@@ -246,7 +246,7 @@ export function useImagePickers(
               ...CameraActions[1].options,
             },
             selectedPhotos => {
-              setPhotos(newImages.concat(selectedPhotos));
+              callback(newImages.concat(selectedPhotos));
             },
           ),
       },
@@ -254,7 +254,11 @@ export function useImagePickers(
     ]);
   };
 
-  const handleImagePress = (imageIndex: number, images: PhotoType[]) => {
+  const handleImagePress = (
+    imageIndex: number,
+    images: PhotoType[],
+    callback: (newPhotos: PhotoType[]) => void,
+  ) => {
     const newImages = images;
 
     Alert.alert('사진을 수정하시겠습니까?', '', [
@@ -269,7 +273,7 @@ export function useImagePickers(
             },
             selectedPhotos => {
               newImages[imageIndex] = selectedPhotos[0];
-              setPhotos(newImages);
+              callback(newImages);
             },
           ),
       },
@@ -284,190 +288,23 @@ export function useImagePickers(
             },
             selectedPhotos => {
               newImages[imageIndex] = selectedPhotos[0];
-              setPhotos(newImages);
+              callback(newImages);
             },
           ),
       },
       {
         text: '삭제',
-        // 삭제
-        onPress: () => setPhotos(newImages.splice(imageIndex, 1)),
+        onPress: () => {
+          newImages.splice(imageIndex, 1);
+          callback(newImages);
+        },
       },
       { text: '취소', style: 'destructive' },
     ]);
   };
 
-  return [handleAddButtonPress, handleImagePress];
+  return {
+    handleAddButtonPress: handleAddButtonPress,
+    handleImagePress: handleImagePress,
+  };
 }
-
-// const PhotoOptions = ({
-//   buttonLabel,
-//   photo,
-//   setPhoto,
-//   max,
-//   style,
-//   imageStyle,
-//   containerStyle,
-// }: PhotoProps) => {
-//   const CameraActions: Action[] = [
-//     //카메라 & 갤러리 세팅
-//     {
-//       title: '카메라',
-//       type: 'capture',
-//       options: {
-//         selectionLimit: max - (photo === undefined ? 0 : photo.length),
-//         mediaType: 'photo',
-//         includeBase64: false,
-//         maxHeight: 300,
-//         maxWidth: 300,
-//       },
-//     },
-//     {
-//       title: '앨범',
-//       type: 'library',
-//       options: {
-//         selectionLimit: max - (photo === undefined ? 0 : photo.length),
-//         mediaType: 'photo',
-//         includeBase64: false,
-//         maxHeight: 300,
-//         maxWidth: 300,
-//       },
-//     },
-//   ];
-
-//   const onButtonPress = useCallback(
-//     (
-//       type: string,
-//       options: ImagePicker.CameraOptions | ImagePicker.ImageLibraryOptions,
-//     ) => {
-//       if (type === 'capture') {
-//         ImagePicker.launchCamera(options, response => {
-//           if (response.errorCode !== undefined) {
-//             console.log('ImagePicker Error: ', response.errorMessage);
-//           } else if (!response.didCancel) {
-//             const selectedPhotos = response.assets!.map(asset => {
-//               if (asset.uri === undefined)
-//                 throw console.error('ImagePicker error: uri not found');
-//               else
-//                 return {
-//                   fileName: asset.fileName,
-//                   width: asset.width,
-//                   height: asset.height,
-//                   uri: asset.uri,
-//                 };
-//             });
-//             if (photo === undefined) setPhoto(selectedPhotos);
-//             else setPhoto(photo.concat(selectedPhotos));
-//           }
-//         });
-//       } else {
-//         ImagePicker.launchImageLibrary(options, response => {
-//           if (response.errorCode !== undefined) {
-//             console.log('ImagePicker Error: ', response.errorMessage);
-//           } else if (!response.didCancel) {
-//             const selectedPhotos = response.assets!.map(asset => {
-//               if (asset.uri === undefined)
-//                 throw console.error('ImagePicker error: uri not found');
-//               else
-//                 return {
-//                   fileName: asset.fileName,
-//                   width: asset.width,
-//                   height: asset.height,
-//                   uri: asset.uri,
-//                 };
-//             });
-//             if (photo === undefined) setPhoto(selectedPhotos);
-//             else setPhoto(photo.concat(selectedPhotos));
-//           }
-//         });
-//       }
-//     },
-//     [],
-//   );
-
-//   return (
-//     <View style={{ backgroundColor: LIGHTGRAY2, ...containerStyle }}>
-//       <PhotosInput>
-//         <View style={{ flex: 1 }}>
-//           <Carousel
-//             data={
-//               photo === undefined
-//                 ? [undefined]
-//                 : photo.length < max
-//                   ? [photo, undefined]
-//                   : photo
-//             }
-//             renderItem={({
-//               item,
-//               idx,
-//             }: {
-//               item: undefined | PhotoType;
-//               idx: number;
-//             }) => {
-//               return (
-//                 <View key={idx}>
-//                   {item === undefined ? (
-//                     <TouchableOpacity
-//                       style={{
-//                         flexDirection: 'row',
-//                         alignItems: 'center',
-//                         paddingHorizontal: 16,
-//                         backgroundColor: 'LIGHTGRAY',
-//                         borderRadius: 6,
-//                         marginBottom: 20,
-//                         paddingVertical: 6,
-//                         ...style,
-//                       }}
-//                       onPress={() => {
-//                         Alert.alert('사진 선택', '', [
-//                           {
-//                             text: '카메라',
-//                             onPress: () =>
-//                               onButtonPress(
-//                                 CameraActions[0].type,
-//                                 CameraActions[0].options,
-//                               ),
-//                           },
-//                           {
-//                             text: '앨범',
-//                             onPress: () =>
-//                               onButtonPress(
-//                                 CameraActions[1].type,
-//                                 CameraActions[1].options,
-//                               ),
-//                           },
-//                           { text: '취소', style: 'destructive' },
-//                         ]);
-//                       }}>
-//                       {buttonLabel && (
-//                         <>
-//                           <PhotoIcon />
-//                           <Body14M style={{ marginLeft: 10 }}>
-//                             {buttonLabel}
-//                           </Body14M>
-//                         </>
-//                       )}
-//                     </TouchableOpacity>
-//                   ) : (
-//                     <></>
-//                   )}
-//                 </View>
-//                 // item.map((subItem: any) => (
-//                 //   <View style={{ height: 350, paddingLeft: 20, paddingRight: 20, paddingTop: 20 }}>
-//                 //     <ImageBackground
-//                 //       key={subItem.id}
-//                 //       source={{ uri: subItem.uri }}
-//                 //       style={{ width: "auto", height: "100%" }}
-//                 //       alt={subItem.fileName}
-//                 //     />
-//                 //   </View>
-//                 // ))
-//               );
-//             }}
-//             slider={photo !== undefined && photo.length > 0}
-//           />
-//         </View>
-//       </PhotosInput>
-//     </View>
-//   );
-// };
