fed55c5817f7d80c81028830c8630622fe1ac73f
Dindb-dong <dongwook443@yonsei.ac.kr>
2024-09-28 15:35:51 +0900

    9.28(2)

--- src/components/Auth/Reformer/ReformFormSubmit.tsx
+++ src/components/Auth/Reformer/ReformFormSubmit.tsx
@@ -1,11 +1,11 @@
-import { Dimensions, SafeAreaView, View } from 'react-native';
+import { Dimensions, SafeAreaView, Text, View } from 'react-native';
 import { Title20B } from '../../../styles/GlobalText';
 import BottomButton from '../../../common/BottomButton';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { SignupProps } from '../Signup';
+import { SignupProp } from '../Signup';
+import Hand from '../../../assets/common/hand.svg'
 
 
-export default function ReformFormSubmit({ navigation, route }: SignupProps) {
+export default function ReformFormSubmit({ navigation, route }: SignupProp) {
   const { height } = Dimensions.get('screen');
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -13,6 +13,12 @@ export default function ReformFormSubmit({ navigation, route }: SignupProps) {
         <Title20B style={{ marginTop: height * 0.2 }}>
           리폼러 프로필 등록이 완료되었어요!
         </Title20B>
+      </View>
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        <Hand />
+      </View>
+      <View style={{ flex: 0.5, alignItems: 'center' }}>
+        <Text>리폼러 등록 승인까지 평균 24시간 정도 소요됩니다</Text>
       </View>
       <BottomButton
         value="홈으로 가기"
